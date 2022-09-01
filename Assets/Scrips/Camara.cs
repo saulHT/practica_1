7a1d9abd88572391c71e04ac47962c8ba0b71a3e
saulHT <josue.huamant@gmail.com>
2022-09-01 13:52:11 -0500

    practica 2

---
+++ Assets/Scrips/Camara.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = player.position.x;
+        var y = player.position.y + 3;
+
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+}
--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -8,8 +8,16 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer sprite;
     Animator animator;
-
+    bool activa = true;
     public float JumFor = 30;
+    private Vector3 laPositionCkeckpoint;
+
+    private bool dobleSalto;
+    private bool grounded;
+
+    public Transform GroundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
 
     private int animacion_quieto = 0;
     private int animacion_caminar = 1;
@@ -24,18 +32,25 @@ public class Player : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
-
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, checkRadius, whatIsGround);
+    }
     // Update is called once per frame
+
     void Update()
     {
+        if (grounded)
+            dobleSalto = false;
+
         rb.velocity = new Vector2(0,rb.velocity.y);
         changeAnimation(animacion_quieto);
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-6,rb.velocity.y);
-            changeAnimation(animacion_caminar);
             sprite.flipX = true;
+            changeAnimation(animacion_caminar);
         }
         if (Input.GetKey(KeyCode.LeftArrow)&& Input.GetKey(KeyCode.X))
         {
@@ -56,16 +71,61 @@ public class Player : MonoBehaviour
             changeAnimation(animacion_correr);
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        //   if (Input.GetKeyUp(KeyCode.Space))
+        //   {
+        // rb.AddForce(new Vector2(0, jumFor), ForceMode2D.Impulse);
+        //     rb.AddForce(Vector2.up * JumFor, ForceMode2D.Impulse);
+        //      changeAnimation(animacion_saltar);
+        //  }
+
+
+        if (Input.GetKeyDown(KeyCode.Space)&& !dobleSalto && !grounded)
         {
-           // rb.AddForce(new Vector2(0, jumFor), ForceMode2D.Impulse);
-            rb.AddForce(Vector2.up * JumFor, ForceMode2D.Impulse);
-            changeAnimation(animacion_saltar);
+            rb.velocity = new Vector2(rb.velocity.x,JumFor);
+            dobleSalto = true;
         }
+         if (Input.GetKeyDown(KeyCode.Space) &&  !grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumFor);
+            
+        }
+
         if (Input.GetKey(KeyCode.Z))
         {
-
             changeAnimation(animacion_atacar);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name=="A")
+        {
+            transform.position = laPositionCkeckpoint;
+            Debug.Log("A");
+        }
+        if (collision.gameObject.name=="B")
+        {
+            Debug.Log("B");
+            transform.position = laPositionCkeckpoint;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (activa==true)
+        {
+            if (collision.gameObject.tag == "1")
+            {
+                Debug.Log("aparece 1");
+                laPositionCkeckpoint = transform.position;
+            }
+        }
+        
+        if (collision.gameObject.tag=="2")
+        {
+            Debug.Log("aparece 2");
+            laPositionCkeckpoint = transform.position;
+            activa = false;
+            Debug.Log("desactiva 1");
         }
     }
 
